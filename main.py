39f9f2e69a84fe84975849becc07ba15821b46d7
Mohammad Javed Momin <javedmomin99@gmail.com>
2021-11-05 19:06:20 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Welcome to the Multiplication Table Generator")
+i = int(input("For Which Number Would You Like to Get Multiplication Table\n"))
+print("Here you go")
+
+for j in range(1,11):
+  print(f"{i}*{j} = {i * j}")
